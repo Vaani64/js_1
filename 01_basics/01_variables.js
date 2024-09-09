7e3751f8d0cbe1859e9f680b0493c81c357f35e4
Vaani64 <149622465+Vaani64@users.noreply.github.com>
2024-09-09 21:06:33 +0000

    datatype commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="vani@688"
+var accountPassword="12345"
+accountCity="Bhopal"
+
+accountEmail="HDFC@.COM"
+console.log(accountId);
+console.log(accountEmail);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,6 @@
+"use strict";//treat all js code as newer version
+//alert("hello")
+console.log(3+3)
+console.log("vani")
+
+console.log(typeof "vani");
