ac866cf6125108d4e2642cfa57f0b86c02c0daf7
Vaani64 <149622465+Vaani64@users.noreply.github.com>
2024-09-10 21:25:41 +0000

    datatype-summary

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score=33
+console.log(typeof(score))
+
+let Score="123"
+let valueInNumber=Number(Score)
+console.log(valueInNumber)
+
+let loggedIn="veru"
+let BooleanLoggedIn=Boolean(loggedIn)
+console.log(BooleanLoggedIn)
+
